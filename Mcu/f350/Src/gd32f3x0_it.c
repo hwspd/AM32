2eac90d3146a78cbbaf52aa56eb00a9ec00a6bea
hwspd <hwspd@users.noreply.github.com>
2026-09-02 21:43:17 +0800

    Add GD32F350 (Cortex-M4) hardware support
    Adds a new GD32F350 MCU port, mirroring the existing GD32E230 port.
    
    - Mcu/f350/: Cortex-M4 CMSIS core + gd32f3x0.h device header, the
      GD32F3x0 standard peripheral library (Source/Include), GCC+MDK startup,
      gd32f350x8 linker script (AM32 4 KB bootloader layout), GD32F350.svd,
      openocd.cfg + gd32f3x0.cfg target, and the AM32 HAL
      (ADC/IO/comparator/phaseouts/peripherals/eeprom/serial_telemetry/
      systick/WS2812).
    - f350makefile.mk: MCU build fragment (-mcpu=cortex-m4, GD32F350 defines).
    - Makefile: add F350 to MCU_TYPES.
    - Inc/targets.h: HARDWARE_GROUP_GD_F350_A/B, MCU_GDF350 defaults, and a
      REF_F350 reference target.

---
+++ Mcu/f350/Src/gd32f3x0_it.c
@@ -0,0 +1,172 @@
+#include <stdint.h>
+
+extern void transfercomplete();
+extern void PeriodElapsedCallback();
+extern void interruptRoutine();
+extern void doPWMChanges();
+extern void tenKhzRoutine();
+extern void sendDshotDma();
+extern void receiveDshotDma();
+extern void signalEdgeRoutine();
+extern void processDshot();
+extern volatile char send_telemetry;
+extern char telemetry_done;
+extern volatile char servoPwm;
+extern volatile char dshot_telemetry;
+extern volatile char out_put;
+extern volatile char armed;
+
+uint16_t interrupt_time = 0;
+
+#include "gd32f3x0_it.h"
+
+#include "common.h"
+#include "main.h"
+#include "systick.h"
+#include "targets.h"
+#include "WS2812.h"
+
+/*!
+    \brief      this function handles NMI exception
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void NMI_Handler(void) { }
+
+/*!
+    \brief      this function handles HardFault exception
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void HardFault_Handler(void)
+{
+    /* if Hard Fault exception occurs, go to infinite loop */
+    while (1) {
+    }
+}
+
+/*!
+    \brief      this function handles SVC exception
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void SVC_Handler(void) { }
+
+/*!
+    \brief      this function handles PendSV exception
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void PendSV_Handler(void) { }
+
+/*!
+    \brief      this function handles SysTick exception
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void SysTick_Handler(void) { delay_decrement(); }
+
+void DMA_Channel3_4_IRQHandler(void)
+{
+    #ifdef USE_LED_STRIP
+        // Check transfer complete for WS2812
+        if (dma_interrupt_flag_get(LED_DMA_CHANNEL, DMA_INT_FLAG_FTF)) {
+            dma_interrupt_flag_clear(LED_DMA_CHANNEL, DMA_INT_FLAG_G);
+            dma_channel_disable(LED_DMA_CHANNEL);
+            dma_busy = 0;  // Ready for next LED update
+            return;
+        }
+        if (dma_interrupt_flag_get(LED_DMA_CHANNEL, DMA_INT_FLAG_ERR)) {
+            dma_interrupt_flag_clear(LED_DMA_CHANNEL, DMA_INT_FLAG_G);
+            dma_channel_disable(LED_DMA_CHANNEL);
+            dma_busy = 0;  // Ready for next LED update
+            return;
+        }
+    #endif
+
+    if (dshot_telemetry && armed) {
+        DMA_INTC |= DMA_FLAG_ADD(DMA_INT_FLAG_G, INPUT_DMA_CHANNEL);
+        DMA_CHCTL(INPUT_DMA_CHANNEL) &= ~DMA_CHXCTL_CHEN;
+        if (out_put) {
+            receiveDshotDma();
+            compute_dshot_flag = 2;
+        } else {
+            sendDshotDma();
+            compute_dshot_flag = 1;
+        }
+        EXTI_SWIEV |= (uint32_t)EXTI_15;
+        return;
+    }
+
+    if (dma_interrupt_flag_get(INPUT_DMA_CHANNEL, DMA_INT_FLAG_HTF)) {
+        if (servoPwm) {
+            TIMER_CHCTL2(TIMER2) |= (uint32_t)(TIMER_IC_POLARITY_FALLING);
+            dma_interrupt_flag_clear(INPUT_DMA_CHANNEL, DMA_INT_FLAG_HTF);
+        }
+    }
+    if (dma_interrupt_flag_get(INPUT_DMA_CHANNEL, DMA_INT_FLAG_FTF) == 1) {
+        dma_interrupt_flag_clear(INPUT_DMA_CHANNEL, DMA_INT_FLAG_G);
+        dma_channel_disable(INPUT_DMA_CHANNEL);
+        transfercomplete();
+        EXTI_SWIEV |= (uint32_t)EXTI_15;
+    } else if (dma_interrupt_flag_get(INPUT_DMA_CHANNEL, DMA_INT_FLAG_ERR) == 1) {
+        dma_interrupt_flag_clear(INPUT_DMA_CHANNEL, DMA_INT_FLAG_G);
+    }
+}
+
+/**
+ * @brief This function handles ADC and COMP interrupts (COMP interrupts
+ * through EXTI lines 21 and 22).
+ */
+void ADC_CMP_IRQHandler(void)
+{
+    if (exti_interrupt_flag_get(EXTI_21)) {
+        exti_flag_clear(EXTI_21);
+        interruptRoutine();
+    }
+}
+
+/**
+ * @brief This function handles TIM6 global and DAC underrun error interrupts.
+ */
+void TIMER13_IRQHandler(void)
+{
+    timer_interrupt_flag_clear(TIMER13, TIMER_INT_FLAG_UP);
+    tenKhzRoutine();
+}
+
+/**
+ * @brief This function handles TIM14 global interrupt.
+ */
+void TIMER15_IRQHandler(void)
+{
+    interrupt_time = TIMER_CNT(UTILITY_TIMER);
+    timer_interrupt_flag_clear(TIMER15, TIMER_INT_FLAG_UP);
+    PeriodElapsedCallback();
+    interrupt_time = ((uint16_t)TIMER_CNT(UTILITY_TIMER)) - interrupt_time;
+}
+
+void TIMER14_IRQHandler(void) { timer_flag_clear(TIMER14, TIMER_FLAG_UP); }
+
+/**
+ * @brief This function handles USART1 global interrupt / USART1 wake-up
+ * interrupt through EXTI line 25.
+ */
+void USART1_IRQHandler(void) { }
+
+void TIMER2_IRQHandler(void)
+{
+    // sendDshotDma();
+}
+
+void EXTI4_15_IRQHandler(void)
+{
+    exti_flag_clear(EXTI_15);
+
+    processDshot();
+}
---
+++ f350makefile.mk
@@ -0,0 +1,30 @@
+MCU := F350
+PART := GD32F350
+
+TARGETS_$(MCU) := $(call get_targets,$(MCU))
+
+HAL_FOLDER_$(MCU) := $(HAL_FOLDER)/$(call lc,$(MCU))
+
+MCU_$(MCU) := -mcpu=cortex-m4 -mthumb
+LDSCRIPT_$(MCU) := $(wildcard $(HAL_FOLDER_$(MCU))/*.ld)
+
+SRC_BASE_DIR_$(MCU) := \
+	$(HAL_FOLDER_$(MCU))/Drivers/GD32F3x0_standard_peripheral/Source \
+	$(HAL_FOLDER_$(MCU))/Startup
+
+SRC_DIR_$(MCU) := $(SRC_BASE_DIR_$(MCU)) \
+	$(HAL_FOLDER_$(MCU))/Src
+
+CFLAGS_$(MCU) += \
+	-I$(HAL_FOLDER_$(MCU))/Inc \
+	-I$(HAL_FOLDER_$(MCU))/Drivers/CMSIS/Include \
+	-I$(HAL_FOLDER_$(MCU))/Drivers/CMSIS/Core/Include \
+	-I$(HAL_FOLDER_$(MCU))/Drivers/GD32F3x0_standard_peripheral/Include
+
+CFLAGS_$(MCU) += \
+	-DGD32$(MCU) \
+	-D$(PART) \
+	-DUSE_STDPERIPH_DRIVER
+
+
+SRC_$(MCU) := $(foreach dir,$(SRC_DIR_$(MCU)),$(wildcard $(dir)/*.[cs]))
